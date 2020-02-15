ea062304e80b691d1ba234e2364ade80f601b4e6
한재현 <ultimate931218@gmail.com>
2020-02-15 13:52:36 +0900

    store 모듈화

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,7 +1,8 @@
 import Vuex from "vuex"; 
 import Vue from "vue";
-import {fetchNewsList,fetchJobList,fetchAskList }from '../api/index.js'
 
+import mutations from "./mutations.js"
+import actions from "./actions.js"
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
@@ -15,40 +16,6 @@ export const store = new Vuex.Store({
             return state.asks;
         }
     },
-    mutations : {
-        SET_NEWS(state,news){
-            state.news = news ;
-
-        },
-        SET_JOBS(state,jobs){
-            state.jobs = jobs;
-        },
-        SET_ASKS(state,asks){
-            state.asks= asks;
-        }
-    },
-    actions:{
-        FETCH_NEWS({commit}){
-            fetchNewsList().then(({data})=>{
-              commit('SET_NEWS',data);
-               
-            }).catch(error => {
-                console.log(error);
-            })
-        },
-        FETCH_JOBS({commit}){
-           fetchJobList().then(({data})=>{
-               commit('SET_JOBS',data);
-           }).catch(error => {
-               console.log(error);
-           })
-        },
-        FETCH_ASKS({commit}){
-            fetchAskList().then(({data})=>{
-                commit('SET_ASKS',data);
-            }).catch(error=>{
-                console.log(error);
-            })
-        }
-    }
+    mutations,
+    actions
  });
