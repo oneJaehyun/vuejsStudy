c4e8ae3e26096c13555a9c772a459f90bba1d039
한재현 <ultimate931218@gmail.com>
2020-05-12 13:14:29 +0900

    데이터 호출 시점 변경

--- vue-news/src/views/CreateListVeiw.js
+++ vue-news/src/views/CreateListVeiw.js
@@ -7,16 +7,14 @@ export default function createListView(name) {
 
     created() {
       bus.$emit("start:spinner");
-      setTimeout(() => {
-        this.$store
-          .dispatch("FETCH_LIST", this.$route.name)
-          .then(() => {
-            bus.$emit("end:spinner");
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }, 3000);
+      this.$store
+        .dispatch("FETCH_LIST", this.$route.name)
+        .then(() => {
+          bus.$emit("end:spinner");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     // 재사용할 인스턴스(컴포넌트) 옵션들이 들어갈 자리
     render(createElement) {
